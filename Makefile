2e4663ae65f36fa52936df11426fc560c20eb094
Stoian Ivanov <s.ivanov@allterco.com>
2019-10-23 11:47:22 +0300

    tests! speedup! binfile for arm to see size

--- Makefile
+++ Makefile
@@ -15,3 +15,10 @@ acos_table_test: acos_table_test.c acos_table_loop.c
 
 run: $(TARGET)
 	./$(TARGET)
+
+bincode:
+	rm acos_table_loop.o acos_table_loop.bin
+	~/opt/gcc-arm-none-eabi/bin/arm-none-eabi-gcc -c -o acos_table_loop.o  acos_table_loop.c
+	~/opt/gcc-arm-none-eabi/bin/arm-none-eabi-objcopy  -S -O binary acos_table_loop.o acos_table_loop.bin 
+	ls -l acos_table_loop.o acos_table_loop.bin
+	rm acos_table_loop.o acos_table_loop.bin
--- acos_table_test.c
+++ acos_table_test.c
@@ -9,24 +9,61 @@
 
 const FLOAT grad2rad_f = PI/180;
 
-static inline FLOAT grad2rad (uint16_t a) { return  grad2rad_f*a; } ;
+static inline FLOAT grad2rad (uint16_t a) { return  a*grad2rad_f; } ;
+static inline FLOAT rad2grad (FLOAT a) { return  a/grad2rad_f; } ;
 
-static inline uint16_t f2intrep (FLOAT f) { return  f*50000; } //fit enoug of FLOAT intto uint16
+static inline uint16_t f2intrep (FLOAT f) { return  f*50000; } //fit enoug of FLOAT into uint16
+static inline FLOAT intrep2f (uint16_t i) { return  ((FLOAT)i)/((FLOAT)50000.0); } //turn back uint16 to FLOAT 
 
 void gen(void);
 
 //cos must be in range 0 to 1.0  inclusive
 extern uint16_t acos_table(uint16_t cos_v);
 
+const FLOAT step = 1.0/50000.0;
 int main(void) {
-	gen (); return 0;
+	//gen (); return 0;
+	uint16_t preva=-1;
+	FLOAT cos_v=1;
+	while (cos_v>=0) {
+		uint16_t cos_vi=f2intrep(cos_v);
+		uint16_t a=acos_table(cos_vi);
+		if (a!=preva || cos_v==0) {
+			FFLOAT a_chk=(FFLOAT)rad2grad(acos(cos_v));
+			printf("a=%u cos=%.5f(%u) acos(a):%.5f ",(unsigned)a,(FFLOAT)cos_v,(unsigned)cos_vi,a_chk);
+			if(cos_v!=0 && cos_v!=1) {
+				printf(" p:%.5f n:%.5f ",(FFLOAT)rad2grad(acos(cos_v+step)),(FFLOAT)rad2grad(acos(cos_v-step)));
+			}
+			preva=a;
+			if ((uint16_t)a_chk!=a) {
+				if ((uint16_t)(a_chk+0.05)!=a) {
+					printf("!!! ERRORR !!!\n");
+					return -1;
+				} else {
+					printf("OK?\n");
+					//printf("!!! WARN! !!!\n");
+				}
+			} else {
+				printf("OK!\n");
+			}
+		}
+		cos_v-=step;
+		if (cos_v<0 && cos_v!=-step) cos_v=0; 
+	}
 	return 0;
 }
 
 void gen() {
-	for (uint16_t a=1; a<=89; a++) {
-		FLOAT v=cos(grad2rad(a));
-		uint16_t v_i=f2intrep(v);
-		printf("%u, // a=%u cos=%.5f\n",(unsigned)v_i,(unsigned)a,(FFLOAT)v);
+	uint16_t cos_vi=50000;
+	uint16_t prev= 0;
+	while (cos_vi) {
+		FLOAT cos_v=intrep2f(cos_vi);
+		FLOAT a=rad2grad(acos(cos_v));
+		uint16_t ai=(uint16_t)a;
+		if (ai!=prev) {
+			printf("%u, // a=%u(%.5f) cos=%.5f\n",(unsigned)cos_vi,(unsigned)ai,(FFLOAT)a,(FFLOAT)cos_v);
+			prev=ai;
+		} 
+		cos_vi--;
 	}
 }
